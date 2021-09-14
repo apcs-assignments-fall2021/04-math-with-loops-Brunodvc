4634fc5af6b6899cf6108b9b993edc15147515da
brunodovalcallao <24bdovalcallao@athenian.org>
2021-09-13 22:21:09 -0700

    This has been the hardest homework so far easily. But I passed the tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,8 +4,11 @@ public class MyMain {
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int total = 1;
+        for (int i = 1; i < x+1; i++){
+          total *= i;
+        }
+        return total;
     }
 
     // Calculates the value for the math constant e iteratively
@@ -13,18 +16,42 @@ public class MyMain {
     // different from the value Math.E
     public static double calculateE() {
         // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        //has to be within 0.00000000001 of 2.718281828459045
+        double e = 0.0;
+        int counter = 0;
+        while (Math.abs(e-2.718281828459045)>0.00001){
+
+            e += 1/factorial(counter);
+            counter += 1;
+            System.out.println(e);
+
+        }
+
+        return e;
     }
 
     // Calculates the square root iteratively, using the Babylonian method
+    //Let's assume that x is the inputted double
+    //Start with a guess, n. n can be any positive number.
+    //Use the following formula to get a better guess: m = (n + x/n) / 2
+    //As m is a better guess than n, update the value of your guess (set n = m)
+    //Repeat steps 1-4 (with a loop) until the difference between your current n value and your previous n value is less than 0.00001
+    //At this point, n will be a very close approximation of the squareroot.
     public static double babylonian(double x) {
         // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double n = 4.0;
+        double m = (n + x/n) / 2;
+        while (Math.abs(m-Math.sqrt(x))>0.00001){
+            m = (n + x/n) / 2;
+            n = m;
+        }
+        return m ;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println(calculateE());
 
         // YOUR CODE HERE
 
